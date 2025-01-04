39b532bb253ae90acd20eaff1f68d935f2c515ec
Parth2684 <bhosle6006@gmail.com>
2025-01-04 13:17:07 +0530

    atom family recoil practice

---
+++ React_Practice/atom-family/src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import { RecoilRoot, useRecoilState } from 'recoil';
+import { todosAtomFamily } from './atoms';
+
+function App() {
+  return <RecoilRoot>
+    <Todo id={1}/>
+    <br></br>
+    <Todo id={2} />
+  </RecoilRoot>
+}
+
+function Todo({id}) {
+   const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+
+  return (
+    <>
+      {todo.title}
+      <br></br>
+      {todo.description}
+      <br />
+    </>
+  )
+}
+
+export default App
