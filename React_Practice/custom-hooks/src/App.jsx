59641438116af2d8b33e426edabd98a53c4489d8
Karn1306 <karnabhosle6@gmail.com>
2025-01-14 21:26:55 +0530

    custom hooks

--- React_Practice/custom-hooks/src/App.jsx
+++ React_Practice/custom-hooks/src/App.jsx
@@ -3,26 +3,43 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import './App.css'
 
-function useTodos () {
+function useTodos (n) {
   const [todos, setTodos] = useState([])
-
-useEffect(() => {
-  axios.get("https://sum-server.100xdevs.com/todos")
-    .then(res => setTodos(res.data.todos))
-}, [])
-return todos
+  const [loading, setLoading] = useState(true)
+  
+  useEffect(() => {
+    const value = setInterval((n) => {
+      axios.get("https://sum-server.100xdevs.com/todos")
+      .then(res => {
+        setTodos(res.data.todos)
+        setLoading(false)
+      })
+    }, n*1000)
+    axios.get("https://sum-server.100xdevs.com/todos")
+      .then(res => {
+       setTodos(res.data.todos)
+       setLoading(false)
+    })
+    return () => {
+      clearInterval(value)
+    }
+  }, [n])
+  return {todos, loading}
 }
 
 
 function App() {
  
-  const todos = useTodos()
+  const todos = useTodos(5);
   // const [todos, setTodos] = useState([])
 
   // useEffect(() => {
   //   axios.get("https://sum-server.100xdevs.com/todos")
   //     .then(res => setTodos(res.data.todos))
-  // }, []) 
+  // }, [])
+  if(loading){
+    return <div>loading...</div>
+  }
   return (
     <>
       {todos.map(todos => <Track todo={todo} />)}
